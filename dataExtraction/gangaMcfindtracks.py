3ebfbc6512c30d1b41920703150928c339f24013
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2016-01-18 15:36:00 +0000

    Adding info on the daughters of B or D's from the same PV as signal with opposite flavour.

--- dataExtraction/gangaMcfindtracks.py
+++ dataExtraction/gangaMcfindtracks.py
@@ -4,29 +4,36 @@
 
 script   = 'mcfindtracksFinal.py'
 
-def make_template(script, name = "MCFT07") :
+def make_template(script, name = "MCFT07", gridOutput = True) :
     t = JobTemplate( application = Bender( version = "v25r3", module = script ))
     t.name = name
+    bkquery = BKQuery('/MC/2011/Beam3500GeV-2011-MagDown-Nu2-EmNoCuts/Sim05/Trig0x40760037Flagged/Reco12a/Stripping17NoPrescalingFlagged/27163003/ALLSTREAMS.DST')
+    datafiles = bkquery.getDataset()
+    t.inputdata = datafiles
+
+    t.splitter=SplitByFiles(filesPerJob=10, ignoremissing=False, maxFiles=None)
+    NTUPLE_NAME = "mcd02kpi_tracks_ntuple.root"
+    if gridOutput :
+        ntuple = DiracFile(NTUPLE_NAME)
+        histo = DiracFile("mcd02kpi_tracks_histo.root")
+    else :
+        ntuple = LocalFile(NTUPLE_NAME)
+        histo = LocalFile("mcd02kpi_tracks_histo.root")
+        
+    t.outputfiles = [ntuple, histo] 
+
     return t
 
-def make_job(script, name = "MCFT07") :
-
+def make_job(script, name = "MCFT07", gridOutput = True) :
+    if not gridOutput :
+        name += 'Local'
     try :
         t = templates[name]
     except :
-        t = make_template(script)
+        t = make_template(script, name, gridOutput)
 
     j = Job( t, backend = Dirac() )
     #j = Job( t, backend = Local() )
 
-    bkquery = BKQuery('/MC/2011/Beam3500GeV-2011-MagDown-Nu2-EmNoCuts/Sim05/Trig0x40760037Flagged/Reco12a/Stripping17NoPrescalingFlagged/27163003/ALLSTREAMS.DST')
-    datafiles = bkquery.getDataset()
-    j.inputdata = datafiles
-
-    j.splitter=SplitByFiles(filesPerJob=10, ignoremissing=False, maxFiles=None)
-    NTUPLE_NAME = "mcd02kpi_tracks7_ntuple.root"
-    ntuple = DiracFile(NTUPLE_NAME)
-    histo = DiracFile("mcd02kpi_tracks7_histo.root")
-    j.outputfiles = [ntuple, histo] 
 
     return j
